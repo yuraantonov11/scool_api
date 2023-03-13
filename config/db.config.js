d28e338eebee0b9ffaa04fab56e6c601fb0bbe65
Yurii Antonov <yurii.antonov@globallogic.com>
2023-03-13 20:55:41 +0200

    add ssl to db

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,14 @@
+const {readFileSync} = require('fs');
+
 module.exports = {
   host: process.env.AZURE_MYSQL_HOST | "localhost",
   username: process.env.AZURE_MYSQL_USER | "root",
   password: process.env.AZURE_MYSQL_PASSWORD | "pass",
   database: process.env.AZURE_MYSQL_DATABASE | "restapi",
   port: process.env.AZURE_MYSQL_PORT | "3306",
+  ssl: {
+    ca: readFileSync(process.env.PATH_CA_CERTIFICATE),
+  },
   dialect: "mysql",
 };
 
