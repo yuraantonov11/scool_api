6fc51aa5f14d2fddc2810f12122d6cc61bde04e1
Yurii Antonov <yurii.antonov@globallogic.com>
2023-03-13 21:19:52 +0200

    db fix config

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 const {readFileSync} = require('fs');
 
 module.exports = {
-  host: process.env.AZURE_MYSQL_HOST | "localhost",
-  username: process.env.AZURE_MYSQL_USER | "root",
-  password: process.env.AZURE_MYSQL_PASSWORD | "pass",
-  database: process.env.AZURE_MYSQL_DATABASE | "restapi",
-  port: process.env.AZURE_MYSQL_PORT | "3306",
+  host: process.env.AZURE_MYSQL_HOST || "localhost",
+  username: process.env.AZURE_MYSQL_USER || "root",
+  password: process.env.AZURE_MYSQL_PASSWORD || "pass",
+  database: process.env.AZURE_MYSQL_DATABASE || "restapi",
+  port: process.env.AZURE_MYSQL_PORT || "3306",
   dialectOptions: {
     ssl: {
       ca: readFileSync(process.env.PATH_CA_CERTIFICATE).toString(),
