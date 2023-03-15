76418c7683b4b754fc03182f9ee619ebb4fc77c5
Yurii Antonov <yurii.antonov@globallogic.com>
2023-03-15 17:32:23 +0200

    db fix config 3; add sqlite

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,23 @@
 const {readFileSync} = require('fs');
 
-module.exports = {
+const isProduction = process.env.AZURE_MYSQL_HOST;
+// const isProduction = process.env.NODE_ENV === 'production';
+const config = {
   host: process.env.AZURE_MYSQL_HOST || "localhost",
   username: process.env.AZURE_MYSQL_USER || "root",
   password: process.env.AZURE_MYSQL_PASSWORD || "pass",
   database: process.env.AZURE_MYSQL_DATABASE || "restapi",
   port: process.env.AZURE_MYSQL_PORT || "3306",
-  dialectOptions: {
-    ssl: {
-      ca: process.env.PATH_CA_CERTIFICATE ? readFileSync(process.env.PATH_CA_CERTIFICATE).toString() : "",
-    },
-  },
-  dialect: "mysql",
+  dialect: isProduction ? 'mysql' : 'sqlite',
+  storage: isProduction ? undefined : ':memory:',
 };
 
+if(process.env.PATH_CA_CERTIFICATE){
+  config.dialectOptions = {
+    ssl: {
+      ca: readFileSync(process.env.PATH_CA_CERTIFICATE).toString(),
+    },
+  };
+}
+
+module.exports = config;
